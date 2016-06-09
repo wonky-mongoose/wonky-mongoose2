f3b0c3d54ed6727d76e264960c5d55f231408b9b
Alex <alex.lee3851@gmail.com>
2016-06-08 23:31:58 -0700

    Twilio controller

--- src/server/controllers/Twilio.js
+++ src/server/controllers/Twilio.js
@@ -1,11 +1,55 @@
 import fetch from 'isomorphic-fetch';
 import twilio from 'twilio';
+import { load } from 'dotenv';
+
+// Load environment variables from .env file
+load();
 
 const AccessToken = twilio.AccessToken;
 const ConversationsGrant = AccessToken.ConversationsGrant;
+// Hold information for rooms
+const rooms = {};
 
 const getToken = (req, res) => {
-  fetch('/api/token')
+  // Get identity from parameters
+  const identity = req.query.identity;
+  // Create the user number
+  const userNum = (rooms[identity]) ? rooms[identity].userID:0;
+  // Create new id for each user
+  const newId = `${identity}${userNum}`;
+  // Create a new room if it doesn't exist
+  if (!rooms[identity]) rooms[identity] = {
+    userID: 0,
+    owner: newId ,
+  };
+  // Add new user to the room
+  else rooms[identity][newId] = newId;
+  // Increment the userID count for the next user (keeps all new IDS unique)
+  rooms[identity].userID++;
+
+  // Generate token
+  const token = new AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY,
+    process.env.TWILIO_API_SECRET
+  );
+
+  // Assign the generated identity to the token
+  token.identity = newId;
+
+  //grant the access token Twilio Video capabilities
+  var grant = new ConversationsGrant();
+  grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
+  token.addGrant(grant);
+
+  // Serialize the token to a JWT string and include it in a JSON response
+  res
+    .status(200)
+    .type('json')
+    .json({
+      identity: newId,
+      token: token.toJwt()
+    });
 }
 
 export default { getToken };
