6ec6598093c26d3187e97b90b551fba6554c4a6f
Alex <alex.lee3851@gmail.com>
2016-06-09 14:35:36 -0700

    add webcam to room and add props to room component

--- src/client/components/Room.js
+++ src/client/components/Room.js
@@ -1,26 +1,29 @@
 import React, { PropTypes } from 'react';
-import paper, { Path, PaperScope, Color, Point} from 'paper';
-import { throttle } from 'underscore';
-import io from 'socket.io-client';
+import Webcam from './Webcam';
 import MessageApp from '../containers/MessageApp';
 import Canvas from './Canvas';
 
-export default class Room extends React.Component {
+class Room extends React.Component {
   constructor(props) {
-  	super(props);
-
+    super(props);
   }
 
   render() {
     return (
-      <div className='container'>
-  	  <div className='row'>
-  	  	<h4 className="center grey-text text-darken-4">R O O M  4 2</h4>
-  	    <MessageApp className='col m4 l4'/>
-  	    <Canvas className='col m8 l8'/>
-  	  </div>
-  	  </div>
-    )
+      <div className="container">
+        <div className="row">
+          <h4 className="center grey-text text-darken-4">R O O M  4 2</h4>
+          <Webcam location={this.props.location} className="col s12" />
+          <MessageApp className="col m4 l4"/>
+          <Canvas className="col m8 l8"/>
+        </div>
+      </div>
+    );
   }
-
 }
+
+Room.propTypes = {
+  location: PropTypes.object.isRequired,
+};
+
+export default Room;
