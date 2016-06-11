9ddc6478d83c893d653753f663ba08a301293a44
Alex <alex.lee3851@gmail.com>
2016-06-10 17:26:57 -0700

    update css

--- src/server/server.js
+++ src/server/server.js
@@ -35,6 +35,7 @@ app
   .use(bodyParser.json())
   .use(cookieParser())
   .use(express.static(resolve(__dirname, '../')))
+  .use(express.static(resolve(__dirname, '../../src/client/assets')))
   .use(session({
     secret: 'wonky',
     resave: false,
